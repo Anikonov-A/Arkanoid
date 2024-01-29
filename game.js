0a2595606a2b55ce3b56d0a8b68140239f313665
Anton Anikonov <antonanikonov1@gmail.com>
2024-01-29 11:51:53 +0200

    Drawing static sprites with canvas

--- game.js
+++ game.js
@@ -2,6 +2,9 @@ let game = {
     ctx: null,
     platform: null,
     ball: null,
+    blocks: [],
+    rows: 4,
+    cols: 8,
     sprites: {
         background: null,
         ball: null,
@@ -26,6 +29,16 @@ let game = {
             this.sprites[key].addEventListener('load', onImageLoad);
         }
     },
+    create() {
+        for (let row = 0; row < this.rows; row++) {
+            for (let col = 0; col < this.cols; col++) {
+               this.blocks.push({
+                   x:64 * col + 65,
+                   y:24 * row + 35,
+               })
+            }
+        }
+    },
     run() {
         window.requestAnimationFrame(() => {
             this.render();
@@ -35,11 +48,16 @@ let game = {
         this.ctx.drawImage(this.sprites.background, 0, 0);
         this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
         this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
-        this.ctx.drawImage(this.sprites.block,0,0);
+
+        for (let block of this.blocks) {
+            this.ctx.drawImage(this.sprites.block, block.x, block.y);
+        }
+
     },
     start() {
         this.init();
         this.preload(() => {
+            this.create()
             this.run()
         });
     }
@@ -54,6 +72,7 @@ game.ball = {
     width: 20,
     height: 20
 }
+
 
 window.addEventListener('load', () => {
     game.start();
